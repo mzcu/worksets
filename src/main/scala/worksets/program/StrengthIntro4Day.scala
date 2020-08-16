8bbb04f9af484dc454b5e3b9d3eec7d484053822
Milos Cubrilo <milos.cubrilo@gmail.com>
2020-08-16 19:49:24 +0200

    Workout program tweak

--- src/main/scala/worksets/program/StrengthIntro4Day.scala
+++ src/main/scala/worksets/program/StrengthIntro4Day.scala
@@ -9,21 +9,19 @@ import worksets.workouts.Dsl._
 @SuppressWarnings(Array("org.wartremover.warts.All"))
 class StrengthIntro4Day(implicit private val workoutHistory: WorkoutHistory) extends WorkoutGenerator {
 
-  private val sixToEightRange = 6 to 8
-
   private val squatDay: WorkoutDay = { date =>
     (
       workout on date
-        exercise CompetitionSquat worksetByE1RM 85.pct x 1 worksetRelative 90.pct x 4 sets 3
+        exercise CompetitionSquat worksetByE1RM 87.pct x 1 worksetRelative 92.pct x 4 sets 3
         exercise CompetitionBench worksetByE1RM 78.pct x 3 loadDrop 3
-        exercise WideGripBench worksetByE1RM 72.pct x 6 repeat 2
+        exercise WideGripBench worksetByE1RM 75.pct x 6 repeat 2
       )
   }
 
   private val deadLiftDay: WorkoutDay = { date =>
     (
       workout on date
-        exercise CompetitionDeadlift worksetByE1RM 90.pct x 1 worksetRelative 90.pct x 3 sets 3
+        exercise CompetitionDeadlift worksetByE1RM 90.pct x 1 worksetRelative 93.pct x 3 sets 3
         exercise PendlayRowStandard workset 90.0.kg x 4 at Rpe8 worksetRelative 90.pct x 4 sets 3
       )
   }
