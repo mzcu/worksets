ff4afaa2c9cb8c1055fbd7dce09a4adf95d2c8b3
Milos Cubrilo <milos.cubrilo@gmail.com>
2020-09-27 11:40:19 +0200

    Add weekly volume report

--- src/main/scala/worksets/cli/ShowReport.scala
+++ src/main/scala/worksets/cli/ShowReport.scala
@@ -20,20 +20,26 @@ object ShowReport:
     val allWorkouts = ObjectStore.load()
     allWorkouts.filter(_.sets.exists(_.completed))
 
-  @SuppressWarnings(Array("org.wartremover.warts.Any"))
   def lastWorkouts(n: Int = 5): Unit =
     println(s"Last ${n.toString} workout(s)")
     completedWorkouts.map(_.show).foreach(Console.println)
+  
+  def weeklyVolumePerExercise(workouts: Int = 30) =
+    completedWorkouts.takeRight(workouts).flatMap { w =>
+      val weekString = w.date.format(YearWeekFormatter)
+      WorkoutStats.volumePerExercise(w).map(v => (v._1, weekString, v._2))
+    }.groupBy(_._1)
+    
+  def showWeeklyVolume() =
+    val weeklyVolume = weeklyVolumePerExercise(30).values.flatMap(_.map(v => (v._2, v._3))).groupMapReduce(_._1)(_._2)(_ + _).toList
+    weeklyVolume.sortBy(_._1).foreach((wk, weight) => println(s"$wk: ${weight.show}"))
 
   def volumeProgression(workouts: Int = 10): Unit =
 
     val textBuffer = new TextBuffer
     textBuffer.colMode()
 
-    val volumePerExercise = completedWorkouts.takeRight(workouts).flatMap { w =>
-      val weekString = w.date.format(YearWeekFormatter)
-      WorkoutStats.volumePerExercise(w).map(v => (v._1, weekString, v._2))
-    }.groupBy(_._1)
+    val volumePerExercise = weeklyVolumePerExercise(workouts)
 
     val exerciseVolumePerWeek = volumePerExercise.map { case (exercise, volumeData) =>
       val weekGroups = volumeData.groupMapReduce(_._2)(_._3)(_ + _).toList.sortBy(_._1).map(t => s"${t._1}: ${t._2.show}").mkString("\n")
@@ -65,6 +71,10 @@ object ShowReport:
     println(textBuffer.format)
 
   def main(args: Array[String]): Unit =
-    e1rm()
-    volumeProgression()
+    println("E1RM per exercise")
+    e1rm(20)
+    println("Volume per exercise")
+    volumeProgression(20)
+    println("Weekly volume")
+    showWeeklyVolume()
 
